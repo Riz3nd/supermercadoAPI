ecd410368c2790d280b8c4136c7270d74f624492
Rizend <rizend96@outlook.com>
2022-04-20 19:51:17 -0500

    Primer parcial ultimo commit

--- src/main/java/com/supermercado/producto/repository/ProductRepository.java
+++ src/main/java/com/supermercado/producto/repository/ProductRepository.java
@@ -4,6 +4,5 @@ import com.supermercado.producto.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-
+    Product findProductById(int id);
 }
